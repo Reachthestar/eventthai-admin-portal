15de19f608246aded129c6496d3f932862749973
Reachthestar <nystar.aus@gmail.com>
2026-03-06 14:38:41 +0700

    hot fix : added runtime edge

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const runtime = "edge";
+
 import { Header } from "@/components/header";
 import { UserTable } from "@/components/user-table";
 import { Users } from "lucide-react";
